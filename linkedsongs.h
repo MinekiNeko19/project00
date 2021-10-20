0d0b84e4c85fc9f05a159e750e83618adacd1ce3
Sarah Leong <sleong20@stuy.edu>
2021-10-20 10:55:28 -0400

    added more parameters

--- linkedsongs.h
+++ linkedsongs.h
@@ -8,13 +8,13 @@ struct song_node{
     struct song_node *next;
 };
 
-struct song_node * add_song(char n[100], char a[100]);
+// struct song_node * add_song(char n[100], char a[100]);
 struct song_node * insert_front(struct song_node * list, char n[100], char a[100]);
 struct song_node * order_list(struct song_node * list);
-// helper
-void print_list();
-struct song_node * find_artist();
-struct song_node * find_song();
-struct song_node * random_song();
-struct song_node * remove_song();
-struct song_node * free_list();
+struct song_node * compare_songs(struct song_node * s1, struct song_node * s2); // helper
+void print_list(struct song_node * list);
+struct song_node * find_song(struct song_node * list, char a[100], char s[100]);
+struct song_node * find_artist(struct song_node * list, char a[100]);
+struct song_node * random_song(struct song_node * list);
+struct song_node * remove_song(struct song_node * list, char a[100], char s[100]);
+struct song_node * free_list(struct song_node * list);
