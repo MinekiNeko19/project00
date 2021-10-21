ed15b995d9f55eb35e4e84364e74dfd2ee636ecb
Sarah Leong <sleong20@stuy.edu>
2021-10-21 19:14:00 -0400

    changed the testing list and working on compare_song

--- linkedsongs.c
+++ linkedsongs.c
@@ -1,16 +1,23 @@
 #include "linkedsongs.h"
 
 int main(){
-    struct song_node * vibe = NULL;
-    printf("\nTesting print_list:\n");
-    vibe = insert_front(vibe, "mokita", "colorblind");
-    vibe = insert_front(vibe, "fun.", "we are young");
-    vibe = insert_front(vibe, "Ed Sheeran", "Happier");
-    print_list(vibe);
+    struct song_node * dw;
+    printf("\nTesting print_list:\n"); // change some later to test capitalization
+    dw = insert_front(dw, "radiohead", "street spirit (fade out)");
+    dw = insert_front(dw, "radiohead", "paranoid android");
+    dw = insert_front(dw, "pink floyd", "time");
+    dw = insert_front(dw, "pearl jam", "yellow ledbetter");
+    dw = insert_front(dw, "pearl jam", "even flow");
+    dw = insert_front(dw, "pearl jam", "alive");
+    dw = insert_front(dw, "ac/dc", "thunderstruck");
+    print_list(dw);
 
     printf("\nTesting find_song and print_song:\n");
-    print_song(find_song(vibe, "mokita", "colorblind"));
-    print_song(find_song(vibe, "yeet", "delete"));
+    print_song(find_song(dw, "pearl jam", "even flow"));
+    print_song(find_song(dw, "yeet", "delete"));
+
+    printf("\nTesting compare_song and order_list:\n");
+    print_song(compare_songs(dw));
     
     return 0;
 }
@@ -18,7 +25,7 @@ int main(){
 void print_list(struct song_node * list) {
     struct song_node * temp = list;
     while(temp) {
-        printf("%s: %s | ", temp -> artist, temp -> name);
+        printf("{%s, %s} | ", temp -> artist, temp -> name);
         temp = temp -> next; 
     }
     printf("\n");
@@ -33,9 +40,41 @@ struct song_node * insert_front(struct song_node * list, char a[100], char s[100
     return list;
 }
 
+// struct song_node * order_list(struct song_node * list); // trying for quicksort
+// struct song_node * compare_songs(struct song_node * s1, struct song_node * s2) { // helper, change later to take into account capitalization
+//     struct song_node * temp;
+//     char one[100];
+//     char two[100];
+//     strcpy(one, s1-> artist);
+//     strcpy(two, s2 -> artist);
+
+//     if (one[0] < two[0]) {
+//         temp = s1;
+//         s1 -> next = s2->next;
+        
+        
+//     }
+// }
+
+struct song_node * compare_songs(struct song_node * prev) { // helper, change later to take into account capitalization
+    struct song_node * s1 = prev -> next;
+    struct song_node * s2 = s1 -> next;
+    char one[100];
+    char two[100];
+    strcpy(one, s1-> artist);
+    strcpy(two, s2 -> artist);
+
+    if (one[0] < two[0]) { // prev s1 s2 -> prev s2 s1
+        s1 -> next = s2 -> next;
+        prev -> next = s2;
+        s2 -> next = s1;
+    }
+    return prev -> next;
+}
+
 void print_song(struct song_node * song) {
     if(song){
-        printf("%s: %s\n", song -> artist, song -> name); 
+        printf("{%s, %s}\n", song -> artist, song -> name); 
     }
 }
 
@@ -51,3 +90,15 @@ struct song_node * find_song(struct song_node * list, char a[100], char s[100]) 
     printf("%s by %s not found!\n", a, s);
     return NULL;
 }
+
+// struct song_node * find_artist(struct song_node * list, char a[100]) { // do order list first before testing this
+//     struct song_node * temp = list;
+//     while (temp) {
+//         if (!strcmp(temp -> artist,a)) {
+//             return temp;
+//         }
+//         temp = temp -> next;
+//     }
+//     printf("%s by %s not found!\n", a, s);
+//     return NULL;
+// }
