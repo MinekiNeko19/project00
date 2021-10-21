8b3feba401e794f9db410a2eda2ffc697116b6ec
Sarah Leong <sleong20@stuy.edu>
2021-10-21 15:22:15 -0400

    tested and fixed find song and helper function

--- linkedsongs.c
+++ linkedsongs.c
@@ -1,5 +1,20 @@
 #include "linkedsongs.h"
 
+int main(){
+    struct song_node * vibe = NULL;
+    printf("\nTesting print_list:\n");
+    vibe = insert_front(vibe, "mokita", "colorblind");
+    vibe = insert_front(vibe, "fun.", "we are young");
+    vibe = insert_front(vibe, "Ed Sheeran", "Happier");
+    print_list(vibe);
+
+    printf("\nTesting find_song and print_song:\n");
+    print_song(find_song(vibe, "mokita", "colorblind"));
+    print_song(find_song(vibe, "yeet", "delete"));
+    
+    return 0;
+}
+
 void print_list(struct song_node * list) {
     struct song_node * temp = list;
     while(temp) {
@@ -18,13 +33,21 @@ struct song_node * insert_front(struct song_node * list, char a[100], char s[100
     return list;
 }
 
-int main(){
-    struct song_node * vibe = NULL;
-    printf("\nTesting print_list:\n");
-    vibe = insert_front(vibe, "mokita", "colorblind");
-    vibe = insert_front(vibe, "fun.", "we are young");
-    vibe = insert_front(vibe, "Ed Sheeran", "Happier");
-    print_list(vibe);
-    
-    return 0;
+void print_song(struct song_node * song) {
+    if(song){
+        printf("%s: %s\n", song -> artist, song -> name); 
+    }
+}
+
+struct song_node * find_song(struct song_node * list, char a[100], char s[100]) {
+    struct song_node * temp = list;
+    while(temp) {
+        if(!strcmp(temp -> artist,a) && !strcmp(temp -> name,s)) {
+            // print_song(temp);
+            return temp;
+        }
+        temp = temp->next;
+    }
+    printf("%s by %s not found!\n", a, s);
+    return NULL;
 }
