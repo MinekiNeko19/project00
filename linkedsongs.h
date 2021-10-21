5d1bd59e3911be06e762bd999b2726236cd672ed
Sarah Leong <sleong20@stuy.edu>
2021-10-21 15:18:36 -0400

    added a new helper i think

--- linkedsongs.h
+++ linkedsongs.h
@@ -13,6 +13,7 @@ struct song_node * insert_front(struct song_node * list, char a[100], char s[100
 struct song_node * order_list(struct song_node * list);
 struct song_node * compare_songs(struct song_node * s1, struct song_node * s2); // helper
 void print_list(struct song_node * list);
+void print_song(struct song_node * song); // helper
 struct song_node * find_song(struct song_node * list, char a[100], char s[100]);
 struct song_node * find_artist(struct song_node * list, char a[100]);
 struct song_node * random_song(struct song_node * list);
