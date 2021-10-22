59fb0ff69bef55578e382680885ed43c333f42e7
Sarah Leong <sleong20@stuy.edu>
2021-10-21 20:50:10 -0400

    changing compare_songs to return the bigger node

--- linkedsongs.c
+++ linkedsongs.c
@@ -3,12 +3,12 @@
 int main(){
     struct song_node * dw;
     printf("\nTesting print_list:\n"); // change some later to test capitalization
-    dw = insert_front(dw, "radiohead", "street spirit (fade out)");
-    dw = insert_front(dw, "radiohead", "paranoid android");
-    dw = insert_front(dw, "pink floyd", "time");
     dw = insert_front(dw, "pearl jam", "yellow ledbetter");
+    dw = insert_front(dw, "radiohead", "paranoid android");
     dw = insert_front(dw, "pearl jam", "alive");
+    dw = insert_front(dw, "radiohead", "street spirit (fade out)");
     dw = insert_front(dw, "ac/dc", "thunderstruck");
+    dw = insert_front(dw, "pink floyd", "time");
     dw = insert_front(dw, "pearl jam", "even flow");
     print_list(dw);
 
@@ -17,8 +17,9 @@ int main(){
     print_song(find_song(dw, "yeet", "delete"));
 
     printf("\nTesting compare_song and order_list:\n");
-    print_song(compare_songs(dw));
+    print_song(compare_songs(dw, dw->next));
     print_list(dw);
+    // dw = order_list(dw);
     
     return 0;
 }
@@ -41,43 +42,20 @@ struct song_node * insert_front(struct song_node * list, char a[100], char s[100
     return list;
 }
 
-// struct song_node * order_list(struct song_node * list); // nvm we'll do the slow bubble sort
-// struct song_node * compare_songs(struct song_node * s1, struct song_node * s2) { // helper, change later to take into account capitalization
-//     struct song_node * temp;
-//     char one[100];
-//     char two[100];
-//     strcpy(one, s1-> artist);
-//     strcpy(two, s2 -> artist);
+// struct song_node * order_list(struct song_node * list){
 
-//     if (one[0] < two[0]) {
-//         temp = s1;
-//         s1 -> next = s2->next;
-        
-        
-//     }
 // }
 
-struct song_node * compare_songs(struct song_node * s1) { // helper, change later for capitalization
-    struct song_node * s2 = s1 -> next;
-    struct song_node * temp;
-    char one[100];
-    char two[100];
-    strcpy(one, s1-> artist);
-    strcpy(two, s2 -> artist);
-
-    if (strcmp(one, two)) { // prev s1 s2 -> prev s2 s1
-        temp = s1;
-        strcpy(temp -> artist, s1 -> artist);
-        strcpy(temp -> name, s1 -> name);
-        temp -> next = s1 -> next;
-        strcpy(s1 -> artist, s2 -> artist);
-        strcpy(s1 -> name, s2 -> name);
-        s1 -> next = s2 -> next;
-        strcpy(s2 -> artist, temp -> artist);
-        strcpy(s2 -> name, temp -> name);
-        s2 -> next = temp -> next;
+struct song_node * compare_songs(struct song_node * s1, struct song_node * s2) { // helper, change later for capitalization
+    if (strcmp(s1 -> artist, s2->artist) < 0) {
+        return s2;
     }
-    return s1 -> next;
+    if (strcmp(s1 -> artist, s2->artist) == 0) {
+        if (strcmp(s1 -> name, s2->name) < 0) {
+            return s2;
+        }
+    }
+    return s1;
 }
 
 void print_song(struct song_node * song) {
