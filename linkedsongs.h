ab122fd2e3d0765f96972e15b1ca604bbf461caf
Sarah Leong <sleong20@stuy.edu>
2021-10-22 00:01:15 -0400

    included time.h

--- linkedsongs.h
+++ linkedsongs.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 struct song_node{
     char name[100];
@@ -16,7 +17,7 @@ void swap(struct song_node * s1, struct song_node * s2); // done
 void print_list(struct song_node * list); // done
 void print_song(struct song_node * song); // helper // done
 struct song_node * find_song(struct song_node * list, char a[100], char s[100]); // done
-struct song_node * find_artist(struct song_node * list, char a[100]);
+struct song_node * find_artist(struct song_node * list, char a[100]); // done
 struct song_node * random_song(struct song_node * list);
 struct song_node * remove_song(struct song_node * list, char a[100], char s[100]);
 struct song_node * free_list(struct song_node * list);
