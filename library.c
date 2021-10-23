04b248666a027bd60334a0a818e3d855f2ab639c
Sarah Leong <sleong20@stuy.edu>
2021-10-22 21:18:50 -0400

    working on add_song, print_lib, make_lib

--- library.c
+++ library.c
@@ -1,19 +1,19 @@
 #include "library.h"
 
 int main() {
-    struct song_node * dw;
+    // struct song_node * dw;
     // testing linkedsongs.c
-    printf("\n==================================\n");
-    printf("\nTesting print_list:\n");
+    // printf("\n==================================\n");
+    // printf("\nTesting print_list:\n\n");
 
-    // scrambled
-    dw = insert_front(dw, "pearl jam", "yellow ledbetter");
-    dw = insert_front(dw, "radiohead", "paranoid android");
-    dw = insert_front(dw, "radiohead", "street spirit (fade out)");
-    dw = insert_front(dw, "ac/dc", "thunderstruck");
-    dw = insert_front(dw, "pearl jam", "even flow");
-    dw = insert_front(dw, "pink floyd", "time");
-    dw = insert_front(dw, "pearl jam", "alive");
+    // // scrambled
+    // dw = insert_front(dw, "pearl jam", "yellow ledbetter");
+    // dw = insert_front(dw, "radiohead", "paranoid android");
+    // dw = insert_front(dw, "radiohead", "street spirit (fade out)");
+    // dw = insert_front(dw, "ac/dc", "thunderstruck");
+    // dw = insert_front(dw, "pearl jam", "even flow");
+    // dw = insert_front(dw, "pink floyd", "time");
+    // dw = insert_front(dw, "pearl jam", "alive");
 
     // ordered
     // dw = insert_front(dw, "radiohead", "street spirit (fade out)");
@@ -24,46 +24,98 @@ int main() {
     // dw = insert_front(dw, "pearl jam", "alive");
     // dw = insert_front(dw, "ac/dc", "thunderstruck");
 
-    print_list(dw);
+    // print_list(dw);
+
+    // printf("\n==================================\n");
+    // printf("\nTesting find_song and print_song:\n\n");
+    // print_song(find_song(dw, "pearl jam", "even flow"));
+    // print_song(find_song(dw, "yeet", "delete"));
+
+    // printf("\n==================================\n");
+    // printf("\nTesting compare_song and order_list:\n\n");
+    // // printf("%d\n",compare_songs(dw, dw->next));
+    // printf("original: \n");
+    // print_list(dw);
+    //     // swap(dw, dw->next);
+    //     // print_list(dw);
+    // dw = order_list(dw);
+    // printf("\nnew: \n");
+    // print_list(dw);
+
+    // printf("\n==================================\n");
+    // printf("\nTesting find_artist\n\n");
+    // print_list(find_artist(dw,"pearl jam"));
+    // print_list(find_artist(dw,"toto"));
+
+    // printf("\n==================================\n");
+    // printf("\nTesting random_song\n\n");
+    // print_song(random_song(dw));
+
+    // printf("\n==================================\n");
+    // printf("\nTesting remove_song\n\n");
+    // print_list(dw);
+    // print_song(remove_song(dw,"pink floyd", "time"));
+    // print_list(dw);
+
+    // printf("\n==================================\n");
+    // printf("\nTesting free_list\n\n");
+    // printf("original: \n");
+    // print_list(dw);
+    // dw = free_list(dw);
+    // printf("\nnew: \n");
+    // print_list(dw);
+
+    // testing library
 
     printf("\n==================================\n");
-    printf("\nTesting find_song and print_song:\n");
-    print_song(find_song(dw, "pearl jam", "even flow"));
-    print_song(find_song(dw, "yeet", "delete"));
+    printf("\nTesting make_lib, add_song, and print_lib\n\n");
+    struct song_node ** dwlib = make_lib();
+    dwlib = add_song(dwlib, "pearl jam", "yellow ledbetter");
+    // dwlib = add_song(dwlib, "radiohead", "paranoid android");
+    // dwlib = add_song(dwlib, "radiohead", "street spirit (fade out)");
+    // dwlib = add_song(dwlib, "ac/dc", "thunderstruck");
+    // dwlib = add_song(dwlib, "pearl jam", "even flow");
+    // dwlib = add_song(dwlib, "pink floyd", "time");
+    // dwlib = add_song(dwlib, "pearl jam", "alive");
+    print_lib(dwlib);
 
-    printf("\n==================================\n");
-    printf("\nTesting compare_song and order_list:\n");
-    // printf("%d\n",compare_songs(dw, dw->next));
-    printf("original: \n");
-    print_list(dw);
-        // swap(dw, dw->next);
-        // print_list(dw);
-    dw = order_list(dw);
-    printf("\nnew: \n");
-    print_list(dw);
-
-    printf("\n==================================\n");
-    printf("\nTesting find_artist\n");
-    print_list(find_artist(dw,"pearl jam"));
-    print_list(find_artist(dw,"toto"));
-
-    printf("\n==================================\n");
-    printf("\nTesting random_song\n");
-    print_song(random_song(dw));
-
-    printf("\n==================================\n");
-    printf("\nTesting remove_song\n");
-    print_list(dw);
-    print_song(remove_song(dw,"pink floyd", "time"));
-    print_list(dw);
-
-    printf("\n==================================\n");
-    printf("\nTesting free_list\n");
-    printf("original: \n");
-    print_list(dw);
-    dw = free_list(dw);
-    printf("\nnew: \n");
-    print_list(dw);
-    
     return 0;
 }
+
+struct song_node ** make_lib() {
+    struct song_node ** temp = calloc(27,sizeof(struct song_node *));
+    return temp;
+}
+
+struct song_node ** add_song(struct song_node ** lib, char a[100], char s[100]) {
+    // Uppercase Letters − A - Z having ASCII values from 65 - 90 where, 65 and 90 are inclusive.
+    // Lowercase Letter − a - z having ASCII values from 97 - 122 where, 97 and 122 are inclusive.
+    struct song_node * temp = lib[a[0] % 97];
+    temp = insert_front(temp, a, s);
+    return lib;
+}
+
+// struct song_node * find_songlib(struct song_node ** lib, char a[100], char s[100]);
+// struct song_node * find_artistlib(struct song_node ** lib, char a[100], char s[100]);
+
+void print_lib(struct song_node ** lib) {
+    struct song_node ** temp = lib;
+    struct song_node * tempsong = lib[0];
+    int i;
+    for (i = 0; i < 27; i++) {
+        if (i != 26) {
+            printf("%c\n", 65+i);
+        } else {
+            printf("Other:\n");
+        }
+        print_list(tempsong);
+        temp = temp+1;
+
+    }
+}
+
+// void print_letter(struct song_node ** lib);
+// void print_artist(struct song_node ** lib);
+// struct song_node * shuffle(struct song_node ** lib, int numSongs);
+// struct song_node ** delete_song(struct song_node ** lib, char a[100], char s[100]);
+// struct song_node ** free_lib(struct song_node ** lib);
