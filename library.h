a663ee1cd8ba4007f2a9a207e7b139973445171f
Sarah Leong <sleong20@stuy.edu>
2021-10-23 06:53:55 -0400

    changed find_artistlib

--- library.h
+++ library.h
@@ -3,7 +3,7 @@
 struct song_node ** make_lib();
 struct song_node ** add_song(struct song_node ** lib, char a[100], char s[100]);
 struct song_node * find_songlib(struct song_node ** lib, char a[100], char s[100]);
-struct song_node * find_artistlib(struct song_node ** lib, char a[100], char s[100]);
+struct song_node * find_artistlib(struct song_node ** lib, char a[100]);
 void print_lib(struct song_node ** lib);
 void print_letter(struct song_node ** lib);
 void print_artist(struct song_node ** lib);
